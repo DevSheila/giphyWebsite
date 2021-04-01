d3f098dd49b7e4dd93fa34cd0afcaac5a4d58826
sheila <devsheilawambui@gmail.com>
2021-04-01 16:42:03 +0300

    adding search method to data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -22,5 +22,12 @@ export class DataService {
     return this.gifs.asObservable()
   }
 
+  searchGifs(gifName:string){
+    return this.http.get(`https://api.giphy.com/v1/gifs/search?q=${gifName}&api_key=${environment.giphyApiKey}&limit=10`).subscribe((response:any)=>{
+      this.gifs.next(response.data)
+    })
+
+  }
+
   
 }
