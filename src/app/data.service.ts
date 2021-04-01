67a80b2895b2c1b38963bc747cc695bbbd64027e
sheila <devsheilawambui@gmail.com>
2021-04-01 16:36:18 +0300

    adding getTrending function to data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,17 +1,22 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { environment} from '../environments/environment'
+import { BehaviorSubject } from 'rxjs';
 
-export const environment = {
-  production: false,
-  giphyApiKey:'aqV5IAt61N8tajgRRs2Ey5hkyPZH7u7y'
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+  gifs = new BehaviorSubject<any>([])
+  constructor(private http :HttpClient) { 
+   
+  }
+  getTrendingGifs(){
+      return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.giphyApiKey}&limit=10`).subscribe((response:any)=>{
+        this.gifs.next(response.data)
+      })
+  }
+
+  
+}
