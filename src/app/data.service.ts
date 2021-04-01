a710908dccb9da3362a77a864908510a1d381a3d
sheila <devsheilawambui@gmail.com>
2021-04-01 16:34:31 +0300

    added api key

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,44 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
-import { environment} from '../environments/environment'
-import { BehaviorSubject } from 'rxjs';
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 
-@Injectable({
-  providedIn: 'root'
-})
-export class DataService {
+export const environment = {
+  production: false,
+  giphyApiKey:'aqV5IAt61N8tajgRRs2Ey5hkyPZH7u7y'
+};
 
-  gifs = new BehaviorSubject<any>([])
-  constructor(private http :HttpClient) { 
-   
-  }
-  getTrendingGifs(){
-      return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.giphyApiKey}&limit=10`).subscribe((response:any)=>{
-        this.gifs.next(response.data)
-      })
-  }
-
-  getMoreTrendingGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/trending?api_key=${environment.giphyApiKey}&limit=100`).subscribe((response:any)=>{
-      this.gifs.next(response.data)
-    })
-  }
-  getCategoriesGifs(){
-    return this.http.get(`https://api.giphy.com/v1/gifs/categories?api_key=${environment.giphyApiKey}&limit=10`);
-  }
-
-  searchGifs(gifName:string){
-    return this.http.get(`https://api.giphy.com/v1/gifs/search?q=${gifName}&api_key=${environment.giphyApiKey}&limit=10`).subscribe((response:any)=>{
-      this.gifs.next(response.data)
-    })
-
-  }
-
-  getGifs(){
-    return this.gifs.asObservable()
-  }
-
-  // getCatGifs(){
-  //   return this.gifs.asObservable()
-  // }
-}
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
